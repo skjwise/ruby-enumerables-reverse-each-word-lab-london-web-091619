5989a6febd7f24a94a8a07290fc097118fdb9131
Sarah Wise <skjwise@hotmail.com>
2019-08-29 16:20:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
   newString = string.split
-  reversed = newString.collect do |words| p words.reverse 
+  reversed = newString.collect do |words| p words.reverse end
 
 end
   
